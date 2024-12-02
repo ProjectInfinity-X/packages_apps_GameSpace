bf28b60d6999628502c05fde6539b85fb2092fc8
Pranav Vashi <neobuddy89@gmail.com>
2024-12-02 10:28:20 +0530

    GameSpace: Fix lock gesture not working for secondary users
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/utils/ScreenUtils.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/utils/ScreenUtils.kt
@@ -83,8 +83,8 @@ class ScreenUtils @Inject constructor(private val context: Context) {
         }
         remoteRecording = null
         if (isGestureLocked) {
-            Settings.System.putInt(context.contentResolver,
-                    Settings.System.LOCK_GESTURE_STATUS, 0)
+            Settings.System.putIntForUser(context.contentResolver,
+                    Settings.System.LOCK_GESTURE_STATUS, 0, UserHandle.USER_CURRENT)
             isGestureLocked = false
         }
     }
@@ -112,8 +112,9 @@ class ScreenUtils @Inject constructor(private val context: Context) {
     var lockGesture = false
         get() = isGestureLocked
         set(enable) {
-            Settings.System.putInt(context.contentResolver,
-                    Settings.System.LOCK_GESTURE_STATUS, if (enable) 1 else 0)
+            Settings.System.putIntForUser(context.contentResolver,
+                    Settings.System.LOCK_GESTURE_STATUS, if (enable) 1 else 0,
+                    UserHandle.USER_CURRENT)
             field = enable
             isGestureLocked = enable
         }
