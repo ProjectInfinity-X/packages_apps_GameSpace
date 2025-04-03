0ad06ada34bece85f9a2b2c72def505a12cb9b3e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-03 18:27:36 +0800

    GameSpace: Add bypass charging feature
    Change-Id: I60506e2545f2ee82596c26962ea93161bac9ef44
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
@@ -18,6 +18,7 @@
 package io.chaldeaprjkt.gamespace.settings
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
@@ -64,6 +65,12 @@ class SettingsFragment : Hilt_SettingsFragment(), Preference.OnPreferenceChangeL
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val isGoogleDevice = Build.MANUFACTURER.equals("Google", ignoreCase = true)
+        val bypassChargePref = findPreference<Preference>("bypass_charge_enabled")
+        if (!isGoogleDevice && bypassChargePref != null) {
+            preferenceScreen.removePreference(bypassChargePref)
+        }
 
         // Game Optimization preferences
         findPreference<SwitchPreferenceCompat>("game_launch_boost")?.apply {
