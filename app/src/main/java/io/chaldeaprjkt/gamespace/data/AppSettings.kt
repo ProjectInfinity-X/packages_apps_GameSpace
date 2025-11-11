c1cdc36f71c2548aa4c1124a8089dcc3beb6bf77
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-11 21:17:10 +0800

    GameSpace: Adding immersive call overlay feature
    Change-Id: Id47d39474d70fed5874488dc120db00fc7a7a294
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
@@ -82,6 +82,10 @@ class AppSettings @Inject constructor(private val context: Context) {
         get() = db.getBoolean(KEY_LOCK_GESTURE, false)
         set(value) = db.edit().putBoolean(KEY_LOCK_GESTURE, value).apply()
 
+    var callOverlayEnabled
+        get() = db.getBoolean(KEY_CALL_OVERLAY_ENABLED, true)
+        set(point) = db.edit().putBoolean(KEY_CALL_OVERLAY_ENABLED, point).apply()
+
     companion object {
         const val KEY_HEADS_UP_DISABLE = "gamespace_heads_up_disabled"
         const val KEY_AUTO_BRIGHTNESS_DISABLE = "gamespace_auto_brightness_disabled"
@@ -93,5 +97,6 @@ class AppSettings @Inject constructor(private val context: Context) {
         const val KEY_ADB_DISABLE = "gamespace_adb_disabled"
         const val KEY_LOCK_GESTURE = "gamespace_lock_gesture"
         const val KEY_MENU_OPACITY = "gamespace_menu_opacity"
+        const val KEY_CALL_OVERLAY_ENABLED = "call_overlay_enabled"
     }
 }
